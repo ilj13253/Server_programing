7d94d72a6f5fa25dc39000e8a69cb65813e32e32
ilj13253 <butackovilj25@gmail.com>
2024-04-03 16:56:46 +0800

    Server/Client

---
+++ Client/ConsoleApp79/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+namespace ConsoleApp79
+{
+    class Program
+    {
+        public static void TCPMain()
+        {
+            TcpClient client = new TcpClient();
+            client.Connect("127.0.0.1", 3522);
+            NetworkStream stream = client.GetStream();
+
+            Console.WriteLine("Введите ваше имя:");
+            string userName = Console.ReadLine();
+
+            byte[] data = Encoding.ASCII.GetBytes(userName);
+            stream.Write(data, 0, data.Length);
+
+            Console.WriteLine("Вы подключены к серверу. Можете начинать общение.");
+
+            // Поток для приема сообщений от сервера
+            byte[] responseData = new byte[1024];
+            while (true)
+            {
+                int bytesRead = stream.Read(responseData, 0, responseData.Length);
+                string message = Encoding.ASCII.GetString(responseData, 0, bytesRead);
+                Console.WriteLine(message);
+            }
+        }
+        static void Main(string[] args)
+        {
+            //TCPMain();
+            UdpClient client = new UdpClient();
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12345);
+
+            // Время, когда нужно совершить звонок (например, 10:00)
+            TimeSpan callTime = new TimeSpan(10, 0, 0);
+
+            while (true)
+            {
+                // Отправка запроса на сервер
+                byte[] requestData = Encoding.ASCII.GetBytes("getTime");
+                client.Send(requestData, requestData.Length, serverEndPoint);
+
+                // Получение ответа от сервера
+                byte[] responseData = client.Receive(ref serverEndPoint);
+                string currentTime = Encoding.ASCII.GetString(responseData);
+
+                Console.WriteLine("Текущее время: " + currentTime);
+
+                // Проверка времени для звонка
+                TimeSpan currentTimeOfDay = DateTime.Now.TimeOfDay;
+                if (currentTimeOfDay.Hours == callTime.Hours && currentTimeOfDay.Minutes == callTime.Minutes)
+                {
+                    Console.WriteLine("Звонок!");
+                    // Здесь можно добавить логику для звонка, например, проигрывание звонка или отправку уведомления
+                }
+
+                // Ожидание перед следующим запросом
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ Server/ConsoleApp80/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace ConsoleApp80
+{
+    class Program
+    {
+        static Dictionary<string, TcpClient> clients = new Dictionary<string, TcpClient>();
+        public static void TwoMain()
+        {
+            TcpListener server = new TcpListener(IPAddress.Any, 3522);
+            server.Start();
+            Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+                Thread clientThread = new Thread(() => HandleClient(client));
+                clientThread.Start();
+            }
+        }
+        static void Main(string[] args)
+        {
+            //TwoMain();
+            UdpClient server = new UdpClient(12345);
+            Console.WriteLine("Сервер запущен. Ожидание запросов...");
+
+            while (true)
+            {
+                IPEndPoint clientEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                byte[] receivedData = server.Receive(ref clientEndPoint);
+                string request = Encoding.ASCII.GetString(receivedData);
+                if (request == "getTime")
+                {
+                    // Получение текущего времени
+                    string currentTime = DateTime.Now.ToString();
+                    byte[] responseData = Encoding.ASCII.GetBytes(currentTime);
+                    server.Send(responseData, responseData.Length, clientEndPoint);
+                }
+            }
+        }
+        static void HandleClient(TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+            string clientId = Guid.NewGuid().ToString();
+            clients.Add(clientId, client);
+            Console.WriteLine("Клиент подключен. ID: " + clientId);
+
+            byte[] data = new byte[256];
+            while (true)
+            {
+                try
+                {
+                    int bytesRead = stream.Read(data, 0, data.Length);
+                    if (bytesRead > 0)
+                    {
+                        string message = Encoding.ASCII.GetString(data, 0, bytesRead);
+                        Console.WriteLine("Получено сообщение от клиента " + clientId + ": " + message);
+
+                        // Отправка сообщения всем клиентам, кроме отправителя
+                        foreach (var kvp in clients)
+                        {
+                            if (kvp.Value != client)
+                            {
+                                byte[] responseData = Encoding.ASCII.GetBytes(message);
+                                kvp.Value.GetStream().Write(responseData, 0, responseData.Length);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Ошибка при чтении данных от клиента " + clientId + ": " + ex.Message);
+                    break;
+                }
+            }
+
+            // Удаление клиента из списка после отключения
+            clients.Remove(clientId);
+            Console.WriteLine("Клиент отключен. ID: " + clientId);
+            client.Close();
+        }
+    }
+}
